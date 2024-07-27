9f84a71380757051434929d54824c5ed0ab702be
widat <widat.it@gmail.com>
2024-07-27 14:22:36 +0700

    add mvn plugin for build jar and try simple java code

--- src/main/java/com/widat/App.java
+++ src/main/java/com/widat/App.java
@@ -1,5 +1,8 @@
 package com.widat;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,30 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Menu:");
+        System.out.println("1. Satu");
+        System.out.println("2. Dua");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+
+        do {
+
+
+            try {
+                choice = scanner.nextInt();
+                if (choice == 1) {
+                    System.out.println("satu");
+                } else if (choice == 2) {
+                    System.out.println("dua");
+                } else {
+                    System.out.println("not found");
+                }
+            } catch (InputMismatchException e){
+                scanner.next();
+            }
+        } while (choice != 3);
+        scanner.close();
+
     }
 }
